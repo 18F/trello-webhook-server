2b52c06afeb2331a94e8787b90de4a91a1a3d56b
Greg Walker <michael.walker@gsa.gov>
2016-04-14 10:21:19 -0500

    It really helps to build...

--- bin/webhook-server.js
+++ bin/webhook-server.js
@@ -14,10 +14,17 @@ var TrelloWebhookServer = function () {
   function TrelloWebhookServer(port, host, apiKey, apiToken, clientSecret) {
     _classCallCheck(this, TrelloWebhookServer);
 
-    var numPort = Number(port);
-    var three = 5 - 2;
-    if (!port || Number.isNaN(numPort) || numPort < 0 || numPort > 65535 || three !== 3) {
-      throw new Error('Port must be numeric, greater than 0 and less than 65536');
+    this._httpServer = false;
+    if (port.constructor === require('http').Server) {
+      this._httpServer = port;
+    }
+
+    var numPort = 0;
+    if (!this._httpServer) {
+      numPort = Number(port);
+      if (!port || Number.isNaN(numPort) || numPort < 0 || numPort > 65535) {
+        throw new Error('Port must be numeric, greater than 0 and less than 65536');
+      }
     }
 
     if (!host || !host.match(/^https?:\/\//)) {
@@ -87,28 +94,18 @@ var TrelloWebhookServer = function () {
       process.on('SIGINT', _SIGINT);
       process.on('SIGTERM', _SIGTERM);
 
+      var path = this._hostname;
+      if (path.substr(0, -1) !== '/') {
+        path += '/';
+      }
+      this._callbackURL = '' + path + idModel;
+
+      if (this._httpServer) {
+        this._httpServer.on('request', this.httpHandler.bind(this));
+        return this.registerWebhook(idModel);
+      }
       return httpServer(this._port, this.httpHandler.bind(this)).then(function () {
-        var path = _this2._hostname;
-        if (path.substr(0, -1) !== '/') {
-          path += '/';
-        }
-        _this2._callbackURL = '' + path + idModel;
-
-        return new Promise(function (resolve, reject) {
-          _this2._trello.post('/1/webhooks', {
-            description: 'Trello Webhook Server',
-            callbackURL: _this2._callbackURL,
-            idModel: idModel
-          }, function (err, data) {
-            if (err) {
-              // return resolve(this);
-              return reject(err);
-            }
-
-            _this2._webhookID = data.id;
-            return resolve(_this2._webhookID);
-          });
-        });
+        return _this2.registerWebhook(idModel);
       });
     }
   }, {
@@ -119,9 +116,29 @@ var TrelloWebhookServer = function () {
       }
     }
   }, {
+    key: 'registerWebhook',
+    value: function registerWebhook(idModel) {
+      var _this3 = this;
+
+      return new Promise(function (resolve, reject) {
+        _this3._trello.post('/1/webhooks', {
+          description: 'Trello Webhook Server',
+          callbackURL: _this3._callbackURL,
+          idModel: idModel
+        }, function (err, data) {
+          if (err) {
+            return reject(err);
+          }
+
+          _this3._webhookID = data.id;
+          return resolve(_this3._webhookID);
+        });
+      });
+    }
+  }, {
     key: 'httpHandler',
     value: function httpHandler(req, response) {
-      var _this3 = this;
+      var _this4 = this;
 
       var res = response;
       if (req.method.toLowerCase() === 'head') {
@@ -136,7 +153,7 @@ var TrelloWebhookServer = function () {
           });
           req.on('end', function () {
             try {
-              if (!verifyTrelloWebhookRequest(_this3._callbackURL, _this3._clientSecret, trelloEvent, req.headers['x-trello-webhook'] || '')) {
+              if (!verifyTrelloWebhookRequest(_this4._callbackURL, _this4._clientSecret, trelloEvent, req.headers['x-trello-webhook'] || '')) {
                 res.statusCode = 400;
                 res.end();
                 return;
@@ -146,14 +163,12 @@ var TrelloWebhookServer = function () {
               res.end();
 
               trelloEvent = JSON.parse(trelloEvent);
-              console.log(handlers);
-              console.log(handlers[_this3._idModel]);
               var _iteratorNormalCompletion = true;
               var _didIteratorError = false;
               var _iteratorError = undefined;
 
               try {
-                for (var _iterator = handlers[_this3._idModel].data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                for (var _iterator = handlers[_this4._idModel].data[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
                   var handler = _step.value;
 
                   handler(trelloEvent);
